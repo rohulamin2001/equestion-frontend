b27905f132095278bd20ea881c0daaf51445418f
rohulamin2001 <rohulamin360@outlook.com>
2026-06-05 16:44:39 +0600

    refactor: update Tailwind CSS classes to use outline-none instead of outline-hidden

--- src/components/animate-ui/components/radix/sheet.jsx
+++ src/components/animate-ui/components/radix/sheet.jsx
@@ -58,7 +58,7 @@ function SheetContent({
         {children}
         {showCloseButton && (
           <SheetClose
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
+            className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
             <XIcon className="size-4" />
             <span className="sr-only">Close</span>
           </SheetClose>
